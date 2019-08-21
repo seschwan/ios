a9107783f6adfa9fda82899cc95b93268a6747f7
Seschwan <seschwan@Seschwans-MacBook-Pro.local>
2019-08-20 21:53:41 -0600

    Project Setup

---
+++ Master Your Money Build Week/Master Your Money Build Week/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Master Your Money Build Week
+//
+//  Created by Seschwan on 8/20/19.
+//  Copyright © 2019 Seschwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
